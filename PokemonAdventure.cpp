1db0340599a24ffbf7c1d690913eebf2cad6cabb
Dom <dmakas27@gmail.com>
2026-06-22 23:03:04 -0500

    Update PokemonAdventure.cpp

--- PokemonAdventure.cpp
+++ PokemonAdventure.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
 
-//Stats
     srand(time(0));
     string user_pokemon;
     string battle1_pokemon;
-    string battle2_pokemon;
-    string battle3_pokemon;
+
+//Stat variables
     int hp;
     int atk;
     int spa;
@@ -16,15 +15,23 @@ int main() {
     int spd;
     int spe;
 
+
+    int opp_hp;
+    int opp_atk;
+    int opp_def;
+    int opp_spa;
+    int opp_spd;
+    int opp_spe;
+
 //All Possible Pokemon in the program
-    "Venasaur",
+    "Venusaur",
     "Charizard",
     "Blastoise",
-    butterfree
-    beedrill
-    pidgeot
-    raticate
     "Snorlax",
+    "Jolteon",
+    "Mew",
+    "Machamp",
+    "Gengar",
 
 //Choose your Pokemon
     cout << "Welcome to the world of Pokemon!" << endl;
@@ -54,4 +61,71 @@ int main() {
     cout << "You obtained " << user_pokemon << "!" << endl;
 
 //Starter stats
+    if (user_pokemon == "Venusaur") {
+        hp = 301;
+        atk = 200;
+        def = 202;
+        spa = 236;
+        spd = 236;
+        spe = 196;
+    }
+    if (user_pokemon == "Charizard") {
+        hp = 297;
+        atk = 204;
+        def = 192;
+        spa = 254;
+        spd = 206;
+        spe = 236;        
+    }
+    if (user_pokemon == "Blastoise") {
+        hp = 299;
+        atk = 202;
+        def = 236;
+        spa = 206;
+        spd = 246;
+        spe = 192;
+    }
+    if (user_pokemon == "Snorlax") {
+        hp = 461;
+        atk = 256;
+        def = 166;
+        spa = 166;
+        spd = 256;
+        spe = 96;
+    }
+    if (user_pokemon == "Jolteon") {
+        hp = 271;
+        atk = 166;
+        def = 156;
+        spa = 256;
+        spd = 296;
+    }
+    if (user_pokemon == "Mew") {
+        hp = 341;
+        atk = 236;
+        def = 236;
+        spa = 236;
+        spd = 236;
+        spe = 236;
+    }
+    if (user_pokemon == "Machamp") {
+        hp = 321;
+        atk = 296;
+        def = 196;
+        spa = 166;
+        spd = 206;
+        spe = 146;
+    }
+    if (user_pokemon == "Gengar") {
+        hp = 261;
+        atk = 166;
+        def = 156;
+        spa = 296;
+        spd = 186;
+        spe = 256;
+    }
 
+//Opponent stats
+    while (battle1_pokemon == "" || battle1_pokemon == user_pokemon) {
+        battle1_pokemon = rand;
+    }
