984cf24d8b69e1cca5beb4ef75e0e201d4a54f67
Dom <dmakas27@gmail.com>
2026-06-22 21:02:57 -0500

    Update PokemonAdventure.cpp

--- PokemonAdventure.cpp
+++ PokemonAdventure.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    
-charizard
+    venasaur
+    charizard
+    blastoise
+    butterfree
+    beedrill
+    pidgeot
+    raticate
+    snorlax
+
+cout << "Welcome to the world of Pokemon!" << endl;
+cout << "Choose your Pokemon:"
+string choice1 = cout << "1) " rand
