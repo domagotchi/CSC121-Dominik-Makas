14cd3bbe0b4321b4b6f10a80d192bb56f1d39d4d
Dom <dmakas27@gmail.com>
2026-06-22 22:10:00 -0500

    Update PokemonAdventure.cpp

--- PokemonAdventure.cpp
+++ PokemonAdventure.cpp
@@ -5,6 +5,10 @@ int main() {
 
 //Stats
     srand(time(0));
+    string user_pokemon;
+    string battle1_pokemon;
+    string battle2_pokemon;
+    string battle3_pokemon;
     int hp;
     int atk;
     int spa;
@@ -12,25 +16,42 @@ int main() {
     int spd;
     int spe;
 
-//All Possible Pokemon
-    venasaur
-    charizard
-    blastoise
+//All Possible Pokemon in the program
+    "Venasaur",
+    "Charizard",
+    "Blastoise",
     butterfree
     beedrill
     pidgeot
     raticate
-    snorlax
+    "Snorlax",
 
-//Choose your starter
+//Choose your Pokemon
     cout << "Welcome to the world of Pokemon!" << endl;
     cout << "Choose your Pokemon:" << endl;
     cout << endl;
     string choice1 = rand;
     string choice2 = rand;
     string choice3 = rand;
+    string choice = "0"
     cout << "1) " << choice1 << endl;
     cout << "2) " << choice2 << endl;
     cout << "3) " << choice3 << endl;
-    string poke;
-    cin >> poke
+    
+    while (choice != "1" && choice != "2" && choice != "3") {
+        cin >> choice;
+        if (choice != "1" && choice != "2" && choice != "3") {
+            cout << "Invalid input..." << endl;
+            cout << "Select your Pokemon by entering '1', '2', or '3'." << endl;
+        }
+    }
+    if (choice == "1") {user_pokemon = choice1}
+    if (choice == "2") {user_pokemon = choice2}
+    if (choice == "3") {user_pokemon = choice3}
+
+    cout << endl;
+    cout << "Congratulations!" << endl;
+    cout << "You obtained " << user_pokemon << "!" << endl;
+
+//Starter stats
+
