05882d6d022656801a277c047848a135a84b438e
Dom <dmakas27@gmail.com>
2026-06-22 21:20:05 -0500

    Update PokemonAdventure.cpp

--- PokemonAdventure.cpp
+++ PokemonAdventure.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
+
+//Stats
     srand(time(0));
+    int hp;
+    int atk;
+    int spa;
+    int def;
+    int spd;
+    int spe;
+
+//All Possible Pokemon
     venasaur
     charizard
     blastoise
@@ -12,9 +22,15 @@ int main() {
     raticate
     snorlax
 
+//Choose your starter
     cout << "Welcome to the world of Pokemon!" << endl;
     cout << "Choose your Pokemon:" << endl;
+    cout << endl;
     string choice1 = rand;
     string choice2 = rand;
     string choice3 = rand;
-    cout << "1) " << choice1 
+    cout << "1) " << choice1 << endl;
+    cout << "2) " << choice2 << endl;
+    cout << "3) " << choice3 << endl;
+    string poke;
+    cin >> poke
