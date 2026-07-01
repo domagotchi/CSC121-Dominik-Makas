f8bbcf1a432fb6a7e4452525261377b342e7e5ad
Dom <dmakas27@gmail.com>
2026-07-01 00:35:59 -0500

    Correct syntax and enhance door choice logic
    Fix various syntax errors and improve game logic.

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <chrono>
 #include <vector>
+// try for pausing suspense --- #include <thread>
 using namespace std;
 
 int main() {
+  
+
   vector<string> outcomes = {
   "death",
   "bubbleshield",
@@ -12,7 +16,7 @@ int main() {
   "$1000",
   "$1000",
   "$1000",
-  "$20,000"
+  "$20000"
   };
 
   char start;
@@ -28,14 +32,10 @@ int main() {
 
   if (start.toupper == "Y") {
     int earnings = 0;
-    
-    string door1 = pokechoices[rand() % pokechoices.size()];
-    string door2 = pokechoices[rand() % pokechoices.size()];
-    string door3 = pokechoices[rand() % pokechoices.size()];
     string dooroutcome = "";
     char doorchoice;
 
-    while (dooroutcome =! "death") {
+    while (dooroutcome != "death") {
       cout << "Three doors stand before you... which will you choose?
       cout << "Door 1 (Press '1')" << endl;
       cout << "Door 2 (Press '2')" << endl;
@@ -44,17 +44,44 @@ int main() {
       cout << endl;
       cin >> doorchoice;
 
+      string door1 = outcomes[rand() % outcomes.size()];
+      string door2 = outcomes[rand() % outcomes.size()];
+      string door3 = outcomes[rand() % outcomes.size()];
+
       if (doorchoice == "1") {dooroutcome = door1;}
       if (doorchoice == "2") {dooroutcome = door2;}
       if (doorchoice == "3") {dooroutcome = door3;}
+      else {
+        cout << "Retreating..." << endl;
+        break;
 
+      if (dooroutcome == "$1000") {
+        cout << "Behind door " << doorchoice << ", you find..." << endl;
+        cout << "$1,000!" << endl;
+        earnings += 1000
+      }
+      if (dooroutcome == "$20000") {
+        cout << "Behind door " << doorchoice << ", you find... oh?" << endl;
+        cout << "JACKPOT!" << endl;
+        cout << "You found $20,000!" << endl;
+        earnings += 20000
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      }
 
+  else {
+    cout << "You live to see another day..." << endl;
+  }
 
-
-
-else {
-cout << "You live to see another day..." << endl;
-}
-
+//Exit message
 
 
